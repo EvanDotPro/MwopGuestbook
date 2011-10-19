3132a0d263b85130aebbbabf13e0afa8c9c9abc1
Evan Coury <me@evancoury.com>
2011-10-19 09:06:12 -0700

    updated config for new di changes

--- configs/module.config.php
+++ configs/module.config.php
@@ -1,48 +1,54 @@
 <?php
 return array(
-    'di' => array( 'instance' => array(
-        'alias' => array(
-            'guestbook'        => 'Guestbook\Controller\GuestbookController',
-            'guestbook-db'     => 'Zend\Db\Adapter\DiPdoMysql',
-            'guestbook-mapper' => 'Guestbook\Model\GuestbookMapper',
-            'guestbook-table'  => 'Guestbook\Model\DbTable\Guestbook',
-        ),
-
-        'preferences' => array(
-            'Zend\Mvc\Router\RouteStack' => 'Zend\Mvc\Router\SimpleRouteStack',
-        ),
-
-        'Guestbook\Controller\GuestbookController' => array(
-            'parameters' => array(
-                'mapper' => 'Guestbook\Model\GuestbookMapper',
-                'router' => 'Zend\Mvc\Router\SimpleRouteStack',
+    'di' => array(
+        'instance' => array(
+            'alias' => array(
+                'guestbook'        => 'Guestbook\Controller\GuestbookController',
+                'guestbook-db'     => 'Zend\Db\Adapter\DiPdoMysql',
+                'guestbook-mapper' => 'Guestbook\Model\GuestbookMapper',
+                'guestbook-table'  => 'Guestbook\Model\DbTable\Guestbook',
             ),
-        ),
 
-        'Guestbook\Model\GuestbookMapper' => array('parameters' => array(
-            'dbTable' => 'Guestbook\Model\DbTable\Guestbook',
-        )),
-
-        'Guestbook\Model\DbTable\Guestbook' => array('parameters' => array(
-            'config' => 'guestbook-db',
-        )),
-
-        'guestbook-db' => array(
-            'parameters' => array(
-                'pdo'    => 'guestbook-pdo',
-                'config' => array(),
+            'preferences' => array(
+                'Zend\Mvc\Router\RouteStack' => 'Zend\Mvc\Router\SimpleRouteStack',
             ),
-        ),
 
-        'Zend\View\PhpRenderer' => array('methods' => array(
-            'setResolver' => array(
-                'resolver' => 'Zend\View\TemplatePathStack',
-                'options'  => array(
-                    'script_paths' => array(
-                        'guestbook' => __DIR__ . '/../views',
+            'Guestbook\Controller\GuestbookController' => array(
+                'parameters' => array(
+                    'mapper' => 'Guestbook\Model\GuestbookMapper',
+                    'router' => 'Zend\Mvc\Router\SimpleRouteStack',
+                ),
+            ),
+
+            'Guestbook\Model\GuestbookMapper' => array(
+                'parameters' => array(
+                    'dbTable' => 'Guestbook\Model\DbTable\Guestbook',
+                ),
+            ),
+
+            'Guestbook\Model\DbTable\Guestbook' => array(
+                'parameters' => array(
+                    'config' => 'guestbook-db',
+                ),
+            ),
+
+            'guestbook-db' => array(
+                'parameters' => array(
+                    'pdo'    => 'guestbook-pdo',
+                    'config' => array(),
+                ),
+            ),
+
+            'Zend\View\PhpRenderer' => array(
+                'parameters' => array(
+                    'resolver' => 'Zend\View\TemplatePathStack',
+                    'options'  => array(
+                        'script_paths' => array(
+                            'guestbook' => __DIR__ . '/../views',
+                        ),
                     ),
                 ),
             ),
-        )),
-    )),
+        ),
+    ),
 );
