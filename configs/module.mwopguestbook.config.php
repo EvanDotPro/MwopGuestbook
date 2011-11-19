ba96c16a4bf0b1487d060fc0ce281910240dd5a4
Evan Coury <me@evancoury.com>
2011-11-19 16:31:00 -0700

    Added disclaimer to avoid confusion about config files

--- configs/module.mwopguestbook.config.php
+++ configs/module.mwopguestbook.config.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * MwopGuestbook Configuration
+ *
+ * If you have a conf.d/ directory set up for your project, you can drop this config file in
+ * it and change the values as you wish.
  */
 $mwopGuestbookSettings = array(
     /**
